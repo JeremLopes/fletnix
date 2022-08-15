3e0ff3bda6b691358f1feb62213fcaf22a46a530
Jérémy Lopes <dev.jlopes@gmail.com>
2022-08-15 19:39:52 +0200

    feat(project): add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.scss";
+import Home from "./pages/home/home";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
